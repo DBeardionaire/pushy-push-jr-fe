071ceb922c9a3d7dbba2682a6caa000c964e33c9
David Beard <DBeardionaire@users.noreply.github.com>
2021-12-02 01:53:16 -0500

    remove pwa runtimeCaching

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
+// const runtimeCaching = require('next-pwa/cache')
 
 /** @type {import('next').NextConfig} */
 module.exports = withPWA({
   reactStrictMode: true,
   pwa: {
     dest: 'public',
-    runtimeCaching,
   },
 })
