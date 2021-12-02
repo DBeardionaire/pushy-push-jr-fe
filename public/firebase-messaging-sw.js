5ca63cb6667f4ffd62e22c79c52e323a985c8262
David Beard <DBeardionaire@users.noreply.github.com>
2021-12-02 18:49:03 -0500

    update sw library refs for compat

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,8 +1,9 @@
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here. Other Firebase libraries
 // are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.5.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/9.5.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.5.0/firebase-messaging-compat.js');
+
 
 var firebaseConfig = {
     apiKey: "AIzaSyBCmQ2vVH907_KRpbPucGpMRyxO6QGT_n0",
@@ -16,10 +17,10 @@ var firebaseConfig = {
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
-firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
-const messaging = firebase.messaging();
+const messaging = firebase.messaging(app);
 
 
